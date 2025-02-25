2dc1aa6f7d3d00cf4d6389c9cfc93f4f176de427
Mohammed Arshad <mohammedarshadsheikh76@gmail.com>
2025-02-25 21:52:30 +0530

    model update

--- lib/tdd/data/model/modle_entities.dart
+++ lib/tdd/data/model/modle_entities.dart
@@ -2,14 +2,8 @@
 import 'package:equatable/equatable.dart';
 
 abstract class Model  extends Equatable{
-  bool? status;
-  String? msg;
-  dynamic data;
-  dynamic flags;
 
-  Model({this.status, this.msg, this.data, this.flags});
-
-  Model.fromJson(Map<String, dynamic> json) ;
+  fromJson(Map<String, dynamic> json) ;
 
   Map<String, dynamic> toJson();
 
