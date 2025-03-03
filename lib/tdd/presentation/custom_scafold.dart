25876b13ae6a4aa69f414fe01302dfcafa5d89f8
Mohammed Arshad <mohammedarshadsheikh76@gmail.com>
2025-03-03 20:01:12 +0530

    Cutome Scafold

---
+++ lib/tdd/presentation/custom_scafold.dart
@@ -0,0 +1,38 @@
+
+import 'package:flutter/material.dart';
+import 'package:tdd_arc/core/util/events-notifiers/error_notifier_container.dart';
+
+class PScafold extends StatefulWidget {
+  final Widget? drawer;
+
+  final BottomNavigationBar? bottomNavigationBar;
+
+  const PScafold({
+    super.key,
+    required this.child,
+    this.displayLogoHead = false,
+    this.appBar,
+    this.drawer,
+    this.bottomNavigationBar,
+  });
+
+  final Widget child;
+  final bool displayLogoHead;
+  final PreferredSizeWidget? appBar;
+  @override
+  State<PScafold> createState() => _PScafoldState();
+}
+
+class _PScafoldState extends State<PScafold> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: widget.appBar,
+        drawer: widget.drawer,
+        bottomNavigationBar: widget.bottomNavigationBar,
+        // backgroundColor: context.theme.backgroundColor,
+        body: CustomNotifier(
+            child: CustomNotifier(child: SafeArea(child: widget.child))));
+  }
+}
