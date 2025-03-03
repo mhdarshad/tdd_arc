afaeab33c4169ce51cbac35e2235faae5f550e87
Mohammed Arshad <mohammedarshadsheikh76@gmail.com>
2025-03-03 18:56:08 +0530

    removed langvage model

--- lib/tdd_arc.dart
+++ lib/tdd_arc.dart
@@ -8,7 +8,6 @@ typedef VelocityStatus = VxStatus;
 typedef GetRequest = Future<RepositoryModel> Function();
 typedef EitherOrFailure<T> = Either<Failure, T>;
  
- export 'package:flutter_localizations/flutter_localizations.dart';
 /// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
