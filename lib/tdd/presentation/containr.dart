af5d815b69577f36faaf014dd4a87814ef1b7c0e
Mohammed Arshad <arshad.cosmostechnosolution@gmail.com>
2025-03-06 03:26:20 +0530

    Mutation call back fixed

--- lib/tdd/presentation/containr.dart
+++ lib/tdd/presentation/containr.dart
@@ -1,31 +1,45 @@
-import 'package:flutter/material.dart' ;
+import 'package:flutter/material.dart';
 import 'package:tdd_arc/core/util/store/store.dart';
 import 'package:tdd_arc/tdd_arc.dart' show VelocityStatus;
 import 'package:velocity_x/velocity_x.dart';
 
 abstract class BaseContainer extends StatelessWidget {
   const BaseContainer({
-    super.key, 
-    required this.builder, 
+    super.key,
+    required this.builder,
     required this.mutations,
   });
-  final Set<Type> mutations;  // Allow passing dynamic mutations
+  final Set<Type> mutations; // Allow passing dynamic mutations
 
   final Widget Function(
-      BuildContext context, ProjectStore store, VelocityStatus state) builder;
+    BuildContext context,
+    ProjectStore store,
+    VelocityStatus state,
+  )
+  builder;
 
   // Abstract method to be implemented by subclasses to handle different states
- // Abstract method to be implemented by subclasses to handle different mutations' states
+  // Abstract method to be implemented by subclasses to handle different mutations' states
   void onMutation(
-      BuildContext context, ProjectStore store, dynamic mutation, VelocityStatus status);
+    BuildContext context,
+    ProjectStore store,
+    VxMutation<VxStore?>  mutation,
+    VelocityStatus status,
+  );
 
   @override
   Widget build(BuildContext context) {
     return VxConsumer<ProjectStore>(
       notifications: Map.fromEntries(
-        mutations.map((mutation) {
-          print("Listning mutations ${mutation.runtimeType}");
-          return MapEntry(mutation.runtimeType, (ctx, store, {status}) => onMutation(ctx, store as ProjectStore, mutation, status!)// Dynamic mutation handling
+        mutations.toList().map((mutation) {
+          return MapEntry(
+            mutation,
+            (ctx, storedata, {status}) => onMutation(
+              ctx,
+              storedata.store as ProjectStore,
+              storedata,
+              status!,
+            ), // Dynamic mutation handling
           );
         }),
       ),
