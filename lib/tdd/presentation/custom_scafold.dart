7b0ff2d550fa98e6e465f2f31958ddb06438ea0e
Mohammed Arshad <arshad.cosmostechnosolution@gmail.com>
2025-04-03 01:48:27 +0530

    custome scaffold optimized

--- lib/tdd/presentation/custom_scafold.dart
+++ lib/tdd/presentation/custom_scafold.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:tdd_arc/core/util/events-notifiers/error_notifier_container.dart';
+import 'package:tdd_arc/tdd/presentation/reponsive_widget.dart';
 
 class PScaffold<T> extends StatefulWidget {
   final Widget? drawer;
@@ -13,13 +14,11 @@ class PScaffold<T> extends StatefulWidget {
     this.displayLogoHead = false,
     this.appBar,
     this.drawer,
-    this.currentPage,
     this.bottomNavigationBar,
   });
 
   final Widget body;
   final bool displayLogoHead;
-  final T? currentPage;
   final PreferredSizeWidget? appBar;
   @override
   State<PScaffold> createState() => _PScafoldState();
@@ -34,11 +33,8 @@ class _PScafoldState extends State<PScaffold> {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       appBar: widget.appBar,
-      drawer: widget.currentPage!=null? NavigationMixin.buildSideNav(context, widget.currentPage):null,
-      bottomNavigationBar:  widget.currentPage!=null?NavigationMixin.buildBottomNav(
-        context,
-        widget.currentPage,
-      ):null,
+      drawer: !ScreenSize.isWeb&&  widget.drawer!=null? widget.drawer:null,
+      bottomNavigationBar:  ScreenSize.isMobile?widget.bottomNavigationBar:null,
       // backgroundColor: context.theme.backgroundColor,
       body: CustomNotifier(
         child: SafeArea(
@@ -47,49 +43,13 @@ class _PScafoldState extends State<PScaffold> {
             mainAxisSize: MainAxisSize.max,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              if( widget.currentPage!=null)
-              NavigationMixin.buildWebNav(context, widget.currentPage),
+              if( ScreenSize.isWeb&&  widget.drawer!=null)
+              widget.drawer!,
               Expanded(child: widget.body),
             ],
           ),
         ),
       ),
     );
-  }
-}
-
-// Define a generic function type where `T` can be any type (e.g., int, String, Enum)
-typedef BuilderParams<T> = Widget Function(BuildContext context, T currentPage);
-
-mixin NavigationMixin {
-  static BuilderParams<dynamic> buildWebNav =
-      (context, currentPage) => SizedBox.shrink();
-  static BuilderParams<dynamic> buildSideNav =
-      (context, currentPage) => SizedBox.shrink();
-  static BuilderParams<dynamic> buildBottomNav =
-      (context, currentPage) => SizedBox.shrink();
-// navigations<T>(Function(BuilderParams<T> builder) webNavBuilder,Function(BuilderParams<T> builder) sideNavBuilder,Function(BuilderParams<T> builder) bottomNavBuilder, ){
-// webNavBuilder
-// }
-
-  static void webNavBuilder<T>(BuilderParams<T> builder) {
-    buildWebNav =
-        kIsWeb
-            ? (context, currentPage) => builder(context, currentPage as T)
-            : (context, currentPage) => SizedBox.shrink();
-  }
-
-  static void sideNavBuilder<T>(BuilderParams<T> builder) {
-    buildSideNav =
-        !kIsWeb
-            ? (context, currentPage) => builder(context, currentPage as T)
-            : (context, currentPage) => SizedBox.shrink();
-  }
-
-  static void bottomNavBuilder<T>(BuilderParams<T> builder) {
-    buildBottomNav =
-        !kIsWeb
-            ? (context, currentPage) => builder(context, currentPage as T)
-            : (context, currentPage) => SizedBox.shrink();
   }
 }
